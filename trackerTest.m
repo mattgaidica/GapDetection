562cdfb6789036688e43ed299c31bbe5bcbf664b
Matt <matt@primestudiosllc.com>
2014-03-29 12:57:43 -0400

    max feature extraction looking good

--- trackerTest.m
+++ trackerTest.m
@@ -1,4 +1,4 @@
-video = VideoReader('NO_PPN BBN Trial 8.avi');
+video = VideoReader('videos/NO_PPN BBN Trial 8.avi');
 videoPlayer = vision.VideoPlayer('Position', [100, 100, 680, 520]);
 
 objectFrame = read(video,1);
@@ -14,11 +14,11 @@ figure, imshow(pointImage), title('Detected interest points');
 markerInserter = vision.MarkerInserter('Shape','Plus','BorderColor','White');
 
 tracker = vision.PointTracker('MaxBidirectionalError', inf,'BlockSize',[21 21]);
-initialize(tracker, points.Location, objectFrame);
+initialize(tracker,points.Location,objectFrame);
 
 allPoints = zeros(size(points,1),2,video.NumberOfFrames);
 
-for i=30:120%video.NumberOfFrames
+for i=1:video.NumberOfFrames
       frame = read(video,i);
       [points, validity] = step(tracker, frame);
       out = insertMarker(frame, points(validity, :), '+');
